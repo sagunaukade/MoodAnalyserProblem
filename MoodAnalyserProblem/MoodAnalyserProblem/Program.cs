c749bfad273b1a27f712c38b1afb9dd948d4c032
sagunaukade <sagunaukade96@gmail.com>
2022-02-12 23:19:53 +0530

    [Saguna] Added Welcome message

---
+++ MoodAnalyserProblem/MoodAnalyserProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the mood analyser problems");
+            Console.ReadLine();
+        }
+    }
+}
