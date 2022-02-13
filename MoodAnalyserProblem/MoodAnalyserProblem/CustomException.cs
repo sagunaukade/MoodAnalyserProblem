7d93b6e0832f42d12beed88fac52a2e59f119451
sagunaukade <sagunaukade96@gmail.com>
2022-02-13 22:31:03 +0530

    [Saguna] Added custom exception

--- MoodAnalyserProblem/MoodAnalyserProblem/CustomException.cs
+++ MoodAnalyserProblem/MoodAnalyserProblem/CustomException.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace MoodAnalyserProblem
 {
-    internal class CustomException
-    {
-    }
+        public class CustomException : Exception
+        {
+            ExceptionType type;
+            public enum ExceptionType
+            {
+                NULL_EXCEPTION, EMPTY_EXCEPTION
+            }
+            public CustomException(ExceptionType type, string message) : base(message)
+            {
+                this.type = type;
+            }
+        }
 }
