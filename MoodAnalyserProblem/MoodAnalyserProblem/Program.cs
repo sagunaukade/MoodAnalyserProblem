a5ea21bfc7fe4575f107a67e81b2771d4839d654
sagunaukade <sagunaukade96@gmail.com>
2022-02-13 22:10:46 +0530

    [Saguna] Added mood analyse happy or sad

--- MoodAnalyserProblem/MoodAnalyserProblem/Program.cs
+++ MoodAnalyserProblem/MoodAnalyserProblem/Program.cs
@@ -11,7 +11,12 @@ namespace MoodAnalyserProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the mood analyser problems");
+
+            //Creating MoodAnalyzer object
+            MoodAnalyser moodAnalyzer = new MoodAnalyser("I am in Happy Mood");
+            Console.WriteLine("Mood is" + moodAnalyzer.AnalyseMood());
             Console.ReadLine();
         }
+      
     }
 }
